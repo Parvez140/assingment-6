9a42ec6d3a41e6d15506bf3a27a7f05c4af787c6
Parvez140 <mohiuddinparvez1@gmail.com>
2022-10-11 23:16:50 +0600

    display search result done

--- app.js
+++ app.js
@@ -7,9 +7,27 @@ const searchPhone = () => {
     // load data
     fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
     .then(res => res.json())
-    .then(data => displayPhone(data))
+    .then(phones => displayPhone(phones.data))
 }
 
 const displayPhone = phones => {
-    console.log(phones)
+    // console.log(phones)
+    const searchResult = document.getElementById('search-result');
+    phones.forEach(phone => {
+        console.log(phone)
+        const div = document.createElement('div');
+        div.classList.add('div');
+        div.innerHTML = `
+            <div onclick="loadPhoneDetails" class="card h-70">
+                <img src="${phone.image}" class="card-img-top p-2" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <p class="card-text">${phone.brand}</p>
+            </div>
+    `;
+    searchResult.appendChild(div);
+    });
+}
+const loadPhoneDetails = phone => {
+    console.log()
 }
